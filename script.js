e91028170653a32e1afb9d56277df2e08f024d3f
Chance Robinson <brighamyoung2@gmail.com>
2022-02-14 18:17:26 -0700

    added feels_like

--- script.js
+++ script.js
@@ -18,11 +18,12 @@ document.getElementById("weatherSubmit").addEventListener("click", function (eve
       results += '<h2>' + json.main.temp + " &deg;F</h2>"
       results += "<p>"
       for (let i = 0; i < json.weather.length; i++) {
-        results += json.weather[i].description
+        results += json.weather[i].description;
         if (i !== json.weather.length - 1)
           results += ", "
       }
       results += "</p>";
+      results += "<h2>Feels like: "+json.main.feels_like+"</h2>";
       document.getElementById("weatherResults").innerHTML = results;
     });
   const url2 = "https://api.openweathermap.org/data/2.5/forecast?q=" + value + ", US&units=imperial" + "&APPID=305a5cbd47e85cf7acdb61c68f952056";
@@ -36,13 +37,15 @@ document.getElementById("weatherSubmit").addEventListener("click", function (eve
         forecast += '<div class="new1"></div>';
         forecast += "<p>" + moment(json.list[i].dt_txt).format('MMMM Do YYYY, h:mm:ss a') + "</p>";
         forecast += "<p>Temperature: " + json.list[i].main.temp + "</p>";
-        forecast += '<img src="http://openweathermap.org/img/w/' + json.list[i].weather[0].icon + '.png"/>'
+        forecast += '<img src="http://openweathermap.org/img/w/' + json.list[i].weather[0].icon + '.png"/>';
+        forecast += "<p>Feels like: "+json.list[i].main.feels_like+"</p>";
       }
       for (let i = 0; i < 2; i++) {
         threeHourForecast += '<div class="new1"></div>';
         threeHourForecast += "<p>" + moment(json.list[i].dt_txt).format('MMMM Do YYYY, h:mm:ss a') + "</p>";
         threeHourForecast += "<p>Temperature: " + json.list[i].main.temp + "</p>";
-        threeHourForecast += '<img src="http://openweathermap.org/img/w/' + json.list[i].weather[0].icon + '.png"/>'
+        threeHourForecast += '<img src="http://openweathermap.org/img/w/' + json.list[i].weather[0].icon + '.png"/>';
+        threeHourForecast += "<p>Feels like: "+json.list[i].main.feels_like+"</p>";
       }
       document.getElementById("forecastResults").innerHTML = forecast;
       document.getElementById("3hourResults").innerHTML = threeHourForecast;
