f6fa74a962482f2479bf9c6ce7dd03e9f799e93c
Chance Robinson <brighamyoung2@gmail.com>
2022-02-17 15:27:20 -0700

    random

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ document.getElementById("weatherSubmit").addEventListener("click", function (eve
         results += '<img src="http://openweathermap.org/img/w/' + json.weather[i].icon + '.png"/>';
       }
       results += '<h2>' + json.main.temp + " &deg;F</h2>";
-      results += "<p>Humidity: "+json.main.humidity+"%</p>";
+      results += "<p>Humidity: " + json.main.humidity + "%</p>";
       results += "<p>"
       for (let i = 0; i < json.weather.length; i++) {
         results += json.weather[i].description;
@@ -24,7 +24,7 @@ document.getElementById("weatherSubmit").addEventListener("click", function (eve
           results += ", "
       }
       results += "</p>";
-      results += "<h2>Feels like: "+json.main.feels_like+" &deg;F</h2>";
+      results += "<h2>Feels like: " + json.main.feels_like + " &deg;F</h2>";
       document.getElementById("weatherResults").innerHTML = results;
     });
   const url2 = "https://api.openweathermap.org/data/2.5/forecast?q=" + value + ", US&units=imperial" + "&APPID=305a5cbd47e85cf7acdb61c68f952056";
@@ -34,19 +34,19 @@ document.getElementById("weatherSubmit").addEventListener("click", function (eve
     }).then(function (json) {
       let forecast = "<h2>5 day Forecast</h2>";
       let threeHourForecast = "<h2>3 hour Forecast</h2>";
-      for (let i = 0; i < json.list.length; i+=8) {
+      for (let i = 0; i < json.list.length; i += 8) {
         forecast += '<div class="new1"></div>';
         forecast += "<p>" + moment(json.list[i].dt_txt).format('MMMM Do YYYY, h:mm:ss a') + "</p>";
         forecast += "<p>Temperature: " + json.list[i].main.temp + "</p>";
         forecast += '<img src="http://openweathermap.org/img/w/' + json.list[i].weather[0].icon + '.png"/>';
-        forecast += "<p>Feels like: "+json.list[i].main.feels_like+"</p>";
+        forecast += "<p>Feels like: " + json.list[i].main.feels_like + "</p>";
       }
       for (let i = 0; i < 2; i++) {
         threeHourForecast += '<div class="new1"></div>';
         threeHourForecast += "<p>" + moment(json.list[i].dt_txt).format('MMMM Do YYYY, h:mm:ss a') + "</p>";
         threeHourForecast += "<p>Temperature: " + json.list[i].main.temp + "</p>";
         threeHourForecast += '<img src="http://openweathermap.org/img/w/' + json.list[i].weather[0].icon + '.png"/>';
-        threeHourForecast += "<p>Feels like: "+json.list[i].main.feels_like+"</p>";
+        threeHourForecast += "<p>Feels like: " + json.list[i].main.feels_like + "</p>";
       }
       document.getElementById("forecastResults").innerHTML = forecast;
       document.getElementById("3hourResults").innerHTML = threeHourForecast;
